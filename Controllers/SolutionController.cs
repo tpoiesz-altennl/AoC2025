47f101b4442eaa8743fd8d6e6b75626f5b689e6e
Tristan Poiesz <tristan.poiesz@altencsp.nl>
2025-12-16 12:00:11 +0100

    Add day 11 solutions

--- Controllers/SolutionController.cs
+++ Controllers/SolutionController.cs
@@ -38,6 +38,9 @@ namespace AoC2025.Controllers
                 case 8:
                     Day8 day8 = new(testInput);
                     return day8.GetSolution1() + "\n" + day8.GetSolution2();
+                case 11:
+                    Day11 day11 = new(testInput);
+                    return day11.GetSolution1() + "\n" + day11.GetSolution2();
                 default:
                     return "404";
             }
